e52b85f26c93311470d2b40babe11aaacd040e59
DAGG3R <parkarsufiyan0902@gmail.com>
2018-04-21 16:12:08 +0530

    UI files added

--- dao/chatlist.go
+++ dao/chatlist.go
@@ -10,27 +10,26 @@ import (
 
 var chatListCollection = GetChatListCollection(session)
 
-// ChatListBox : View of one box in the ChatListchatList
+// ChatListEntry : View of one box in the ChatListchatList
 // Contains information regarding Other user in chat with
-type ChatListBox struct {
+type ChatListEntry struct {
 	ChatID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	DisplayName    string
 	DisplayPicture string
-	IsContact      bool
 }
 
 // ChatList : Structure of Chatlist in MongoDB
 type ChatList struct {
-	ID          bson.ObjectId `json:"id" bson:"_id,omitempty" `
-	ListOfChats []ChatListBox `json:"chatlist" bson:"chatlist" `
+	UserID      bson.ObjectId   `json:"user_id" bson:"_id,omitempty" `
+	ListOfChats []ChatListEntry `json:"chatlist" bson:"chatlist" `
 }
 
 // GetChatList : Returns Chatlist of given userID
-func GetChatList(userID string) ChatList {
+func GetChatList(userID bson.ObjectId) ChatList {
 	var chatlist ChatList
 
 	err := chatListCollection.Find(
-		bson.M{"OwnerID": userID}).One(&chatlist)
+		bson.M{"UserID": userID}).One(&chatlist)
 
 	if err == mgo.ErrNotFound {
 		return ChatList{}
@@ -42,11 +41,11 @@ func GetChatList(userID string) ChatList {
 }
 
 // CreateEmptyChatList : Creates an empty chatList for a new User
-func CreateEmptyChatList(ownerID string) {
-	var chatlistbox []ChatListBox
+func CreateEmptyChatList(ownerID bson.ObjectId) {
+	var chatlistbox []ChatListEntry
 
 	chatlist := ChatList{
-		ID:          bson.ObjectIdHex(ownerID),
+		UserID:      ownerID,
 		ListOfChats: chatlistbox}
 
 	err := chatListCollection.Insert(&chatlist)
@@ -57,4 +56,37 @@ func CreateEmptyChatList(ownerID string) {
 	}
 
 	fmt.Println(chatlist)
+}
+
+// CreateChatListEntry : Creates a ChatListEnry Object
+func CreateChatListEntry(ID bson.ObjectId,
+	displayName string, displayPicture string) ChatListEntry {
+
+	return ChatListEntry{ID, displayName, displayPicture}
+}
+
+// AddChat : Adds a chat instance between user1 and user2
+func AddChat(user1 UserProfile, user2 UserProfile) bool {
+
+	chatID := bson.NewObjectId()
+
+	user1ListEntry := CreateChatListEntry(chatID, user2.DisplayName, "")
+	user2ListEntry := CreateChatListEntry(chatID, user1.DisplayName, "")
+
+	err := chatListCollection.Update(
+		bson.M{"_id": user1.ID},
+		bson.M{"$push": bson.M{"chatlist": user1ListEntry}})
+
+	if err != nil {
+		return false
+	}
+
+	err = chatListCollection.Update(
+		bson.M{"_id": user2.ID},
+		bson.M{"$push": bson.M{"chatlist": user2ListEntry}})
+
+	if err != nil {
+		return false
+	}
+	return true
 }
